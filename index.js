cc0abe33476802930b7c7b62dcaa5c3027d6754c
AORUS <workingonpragram5522@gmail.com>
2025-09-28 12:07:13 +0200

    add cors dependency

--- index.js
+++ index.js
@@ -49,6 +49,16 @@ app.get("/", (req, res) => {
   res.send("Nazil API Service is running 🚀");
 });
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
